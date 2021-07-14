96f8411c16c89ef308beefc3edb257a813c36bfd
easywiny <npavlov1337@gmail.com>
2021-07-14 13:03:46 +0300

    add loop for parsing all tables

--- main.py
+++ main.py
@@ -3,36 +3,39 @@ from bs4 import BeautifulSoup
 import xlsxwriter
 
 
-def get_table(url):
-    response = requests.get(url).text
-    parser = BeautifulSoup(response,"lxml")
-    data = {}
-    table = parser.find("table",attrs={"class": "Data"})
+rating_list = ['Women','Men','U21_Women','U21_Men','U17_Women','U17_Men','U14_Women',
+               'U14_Men','U12_Women','U12_Men','U10_Women','U10_Men','Vet_Women','Vet_Men',
+               'Vet2_Women','Vet2_Men','Vet3_Women','Vet3_Men','Vet4_Women','Vet4_Men']
 
-    headings = []
-    for th in table.find_all("th"):
-        headings.append(str(th.text))
-    content = []
-    for row in table.find_all("tr"):
-        if not row.find_all("th"):
-            content.append([element.text for element in row.find_all('td')])
-    print(headings)
-    print(content)
-    for word in headings:
-        print(type(word))
-
-    rown = 0
-    column = 0
+def get_table():
     workbook = xlsxwriter.Workbook('test.xlsx')
-    worksheet = workbook.add_worksheet()
-    for index, word in enumerate(headings):
-        worksheet.write_string(0,index,word)
-    for row_num, row_data in enumerate(content):
-        for col_num, col_data in enumerate(row_data):
-            worksheet.write_string(row_num+1, col_num, col_data)
+    base_url = 'http://www.iwwfed-ea.org/classic/rl2021/eame/index.php?page=RL&categ='
+    for rating in rating_list:
+        url = base_url+rating
+        response = requests.get(url).text
+        parser = BeautifulSoup(response,"lxml")
+        table = parser.find("table",attrs={"class": "Data"})
+        headings = []
+        for th in table.find_all("th"):
+            headings.append(str(th.text))
+        content = []
+        for row in table.find_all("tr"):
+            if not row.find_all("th"):
+                content.append([element.text for element in row.find_all('td')])
+        for list in content:
+            list.remove('\xa0')
+
+
+        worksheet = workbook.add_worksheet(f'{rating}')
+
+        for index, word in enumerate(headings):
+            worksheet.write_string(0,index,word)
+        for row_num, row_data in enumerate(content):
+            for col_num, col_data in enumerate(row_data):
+                worksheet.write_string(row_num+1, col_num, col_data)
 
     workbook.close()
 
 
 
-get_table('http://www.iwwfed-ea.org/classic/rl2021/eame/index.php?page=RL&categ=Men')
+get_table()
